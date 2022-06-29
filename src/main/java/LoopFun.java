cbe0d5c849e44ab943bf24836eeac17d20f6598a
jd <j.judd.singer@gmail>
2022-06-29 12:18:04 -0400

    first failed quiz

--- src/main/java/LoopFun.java
+++ src/main/java/LoopFun.java
@@ -1,27 +1,46 @@
- 
+import java.util.Scanner;
 
-public class LoopFun
-{
+public class LoopFun {
 
-      /**
-       * Given a number, return the factorial of that number.
-       * For example, given 5, the factorial is 5 x 4 x 3 x 2 x 1 which should return 120.
-       * @param number
-       * @return the factorial of the number
-       */
-      public Integer factorial(Integer number){
+    /**
+     * Given a number, return the factorial of that number.
+     * For example, given 5, the factorial is 5 x 4 x 3 x 2 x 1 which should return 120.
+     *
+     * @param number
+     * @return the factorial of the number
+     */
+
+    // Getting first test to pass, I know I'm on the right track but must move on to other questions
+    public Integer factorial(Integer number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum = number *= i;
+            sum++;
+        }
+
+        return sum;
+//          while(int i <= number){
+//              number = number * i;
+//              i++
+//          }
+//          return number;
+    }
+
+    /**
+     * Given a phrase, get the acronym of that phrase. Acronym is the combination of
+     * the first character of each word in upper case.
+     * For example, given "Ruby on Rails", this method will return "ROR"
+     * @param phrase
+     * @return Upper case string of the first letter of each word
+     */
+//      public String acronym(String phrase) {
+//          phrase.split("");
+//          for (int i = 0; i <= phrase; i++){
+//              System.out.println(i);
+//          }
+
           return null;
-      }
-
-      /**
-       * Given a phrase, get the acronym of that phrase. Acronym is the combination of
-       * the first character of each word in upper case.
-       * For example, given "Ruby on Rails", this method will return "ROR"
-       * @param phrase
-       * @return Upper case string of the first letter of each word
-       */
-      public String acronym(String phrase) {
-          return null;
+}
       }
 
       /**
@@ -37,6 +56,21 @@ public class LoopFun
        * @return the encrypted string by shifting each character by three character
        */
       public String encrypt(String word) {
+//          thinking process LONG VERSION......create string of alphabet
+//          split the alphabet string
+//          run for loop on "words" String
+//          for each character of "words" string compare it to its position in alphabet string + 3
+//          return the new element
+          //Not really sure how to accomplish this
+//          String alphaString = "abcdefghijklmnopqrstuvwxyz";
+//          alphaString.split("");
+//          for( int i = 0; i <= word; i++ )
+//              if(int i = a){
+//          }
+
+
+
+
           return null;
       }
 }
--- src/main/java/StringUtilities.java
+++ src/main/java/StringUtilities.java
@@ -5,7 +5,8 @@ public class StringUtilities {
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+
+        return input;
     }
 
     /**
@@ -14,7 +15,8 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+
+        return (baseValue + valueToBeAdded);
     }
 
     /**
@@ -22,14 +24,33 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        String results = "";
+
+        for (int i = valueToBeReversed.length() - 1; i >= 0; i--){
+            results = results + valueToBeReversed.charAt(i);
+
+        }
+        return results;
+
     }
 
     /**
      * @param word word to get middle character of
      * @return middle character of `word`
      */
+
+//    I know im close, still a beginner and trying to figure it all out, had to comment out "getMiddlechar" to run other test
     public Character getMiddleCharacter(String word) {
+//        int position;
+//        int size;
+//        if (word.length() % 2 == 0){
+//            position = word.length() / 2 - 1;
+//            size = 2;
+//        }else {
+//            position = word.length() / 2;
+//            size = 1;
+//        }
+//        return word.substring(position, position + size);
         return null;
     }
 
@@ -39,6 +60,8 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
+        String newStr = String.replace(charToRemove, " ");
+
         return null;
     }
 
@@ -47,6 +70,16 @@ public class StringUtilities {
      * @return last `word` in sentence
      */
     public String getLastWord(String sentence) {
-        return null;
+        String[] word = sentence.split(" ");
+        for(int i = 0; i <= word.length; i++){
+            System.out.println(word[i].length() - 1);
+        }
+//        sentence.split("");
+//        for(int i = 0; i <= sentence; i++){
+//
+//        }
+        return ;
+
     }
+
 }
